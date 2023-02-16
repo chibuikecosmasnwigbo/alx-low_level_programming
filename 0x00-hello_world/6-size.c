32c578c9465c96a17178ea27df38bd7bcb5eddab
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmail.com>
2023-02-16 07:34:40 -0800

    commit this task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int	main(void)
 	long	int	c;
 	long	long	int	d;
 	float	f;
-printf("size	of	a	char:	%lu bytes(s)\n",	(sizeof(a)));
-printf("size	of	a	int:	%lu	bytes(s)\n",	(sizeof(b)));
-printf("size	of	a	long	int:	%lu	bytes(s)\n",	(sizeof(c)));
-printf("size	of	a	long	long	int:	%lu	bytes(s)\n",	(sizeof(d)));
-printf("size	of	a	float:	%lu	byte(s)\n".	(sizeof(f)));
+printf("size	of	a	char:	%lu bytes(s)\n",	(unsigned	long)sizeof(a));
+printf("size	of	a	int:	%lu	bytes(s)\n",	(unsigned	long)sizeof(b));
+printf("size	of	a	long	int:	%lu	bytes(s)\n",	(unsigned	long)sizeof(c));
+printf("size	of	a	long	long	int:	%lu	bytes(s)\n",	(unsigned	long)sizeof(d));
+printf("size	of	a	float:	%lu	byte(s)\n",	(unsigned	long)sizeof(f));
 return	(0);
 }
