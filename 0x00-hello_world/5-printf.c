0ed2fb01f23fa6d9c9cf9c80d06ba6a609ac800b
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmail.com>
2023-02-16 05:23:05 -0800

    commit this task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include	<stdio.h>
+/**
+ *	main	-	a	program	that	print	a	line	using	printf	function
+ *	return:	0	(success)
+ */
+
+int	main	(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,\n");
+	return	(0);
+}
