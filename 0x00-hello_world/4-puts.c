ef44d097129023497f0aa2d2ef55b8dc149ab8a1
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmail.com>
2023-02-16 05:03:25 -0800

    commit this task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include	<stdio.h>
  /**
-  * main	-	A	program	that	prints	a	line	using	the	puts	function
+  * main:	-	A	program	that	prints	a	line	using	the	puts	function
   * return:	0	(success)
   */
 
