0a8edc24f6f84fa9e28e8d8233696546c3af2890
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmail.com>
2023-02-16 04:44:25 -0800

    commit this task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include	<stdio.h>
 /**
- *main
+ *main	a	program	that	prints	a	line	using	puts	function
  return	(0)	success
  */
-int	main(voi)
+int	main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return	(0);
