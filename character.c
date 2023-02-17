0f146d5d93372191a2bda6c64f3ac4a182dd369c
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmail.com>
2023-02-17 15:06:46 -0800

    commit

---
+++ character.c
@@ -0,0 +1,28 @@
+#include	<stdio.h>
+/**
+ * main	-	Description:	Entry	point
+ *
+ * Return:	0	(success)
+ *
+ */
+
+int	main(void)	/*	Return	void	*/
+
+{
+
+	char	UPPERCASE;	=	()
+	char	LOWERCASE;	=	()
+	putchar((UPPERCASE)	(LOWERCASE));
+
+		if(UPPERCASE	==	'A'	&&	<=	'Z');
+		{
+			printf("%c",	(UPPERCASE));
+			putchar(UPPERCASE);
+		}
+		if(LOWERCASE	==	'a'	&&	<=	'z');
+		{
+			printf("%c",	(LOWERCASE));
+			putchar(LOWERCASE);
+		}
+}
+
