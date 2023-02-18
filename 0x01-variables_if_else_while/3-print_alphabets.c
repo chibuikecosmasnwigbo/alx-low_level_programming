ed81ee14f1d6ff299e6310f4c102c678524890b4
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmail.com>
2023-02-18 15:11:06 -0800

    commit this task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main	-	Description:	Entry	point
+ *
+ * Return:	0	(success)
+ */
+
+int	main(void) /*	Return	nothing	*/
+
+{
+	char	c;
+	char	d;
+
+	c	=	'a';
+	d	=	'A';
+
+	while
+		(c	<=	'a')
+		{
+			putchar(c);
+			c++;
+		}
+	while
+		(d	<=	'A')
+		{
+			putchar(d);
+			c++;
+		}
+	return	(0);
+}
