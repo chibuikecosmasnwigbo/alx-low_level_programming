bcc571c6ed2c898c48003915bcc887cdc07c3a67
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmail.com>
2023-02-24 13:57:10 -0800

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#ifndef main_h
+#define main_h
+
+int putchar.c(char);
+void print_alphabet(void);
+void print_alpahabet_10x(void);
+int _isloweer(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int abs(int);
+int print_last_digits(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
