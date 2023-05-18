3f8e7241bfe728d10045e701f7f9694b2fd4ea49
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmail.com>
2023-05-18 08:21:19 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include	<stdio.h>
+/**
+ * Entry	main
+ *
+ * Description:	use	puts	as	output
+ * 
+ **/
+int	main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\");
+	return(0);
+}
+
