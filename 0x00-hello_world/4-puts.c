e67f7f953cd6a922f43c8c6a0a2907fb937c7b5f
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmail.com>
2023-05-19 02:35:46 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include	<stdio.h>
 /**
- * Entry	main
+ * main	-	a	program	that	print	a	line	using	puts
  *
- * Description:	use	puts	as	output
- * 
- **/
+ * Return:	0	(success)
+ */
 int	main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
