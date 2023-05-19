2d74d7fc57be625496a892a4d54f0eee3e0f46bc
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmail.com>
2023-05-19 02:23:42 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,12 @@
  * Entry:	main
  *
  * Description:	print	formated	output	using	printf
- * */
+ * 
+ * Return	(success)
+ */
 
 int	main(void)
 {
-	printf	("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return	(0);
 }
