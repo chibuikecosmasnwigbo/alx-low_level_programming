307639ac286e1e82f3b7b8a329a01d15114c4cbf
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmail.com>
2023-05-19 09:15:50 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+/*
+ * main	-	write	a	program	that	print	alphabet	in	lowercase
+ *
+ * Return:	0	(success)
+ *
+ */
+
+int	main(void)
+{
+	char	ch	=	'a';
+	char	ch_z	=	'z';
+
+	while(	ch	<=	ch_z)
+	{
+		putchar(ch);
+		ch++;
+		putchar('\n');
+	}
+	return	(0);
+}
