f8007a9d75d8d166f272edaeed9fadacb1080481
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmail.com>
2023-05-19 13:24:25 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include        <stdio.h>
+/*
+ * main:        -       Entry   point
+ *
+ * Return:      Always  0       (success)
+ */
+int     main(void)
+{
+	int     num     =       0;
+	while   (num    <=      0)
+	{
+		putchar(num     +       0);
+		num++;
+		putchar('\n');
+	}
+	return	(0);
+}
