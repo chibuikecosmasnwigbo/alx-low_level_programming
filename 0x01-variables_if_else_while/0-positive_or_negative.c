332efcbc8a0974de60b6d357227d4db93ebedfb4
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmail.com>
2023-05-20 13:37:50 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ int	main(void)
 
 	srand(time(0));
 
-	n       =       rand()-RAND_MAX /       2;
+	n       =       rand()	-	RAND_MAX /       2;
 
 	printf("%d"     n);
-	if(     n       >       0)
+	if	(n       >       0)
 		printf("is      positive");
 
-	if(	n	==	0)
+	if	(n	==	0)
 		printf("is	zero");
-	if(	n	<	o)
+	if	(n	<	o)
 		printf("is	negative");
 
 	return	(0);
