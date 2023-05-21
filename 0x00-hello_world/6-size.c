5ca6d6e359a62b8ccda7ca5203c38405afc7cf77
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmail.com>
2023-05-21 08:54:54 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include	<stdio.h>
+/*
+ * main	-	Entry
+ *
+ * return:	Always	0	(success)
+ *
+ */
+int	main(void)
+{
+	int	i;
+	char	c;
+	long	int	k;
+	long	long	int	j;
+	float	m;
+
+	printf("size	of	int:%d",	(sizeof(i))"\n");
+	printf("size	of	char:%c",	(sizeof(c))"\n");
+	printf("size	of	long	int:%ld",	(sizeof(k))"\n");
+	printf("size	of	long	long	int:%lld",	(sizeof(j))"\n");
+	printf("size	of	float:%f",	(sizeof(m))"\n");
+
