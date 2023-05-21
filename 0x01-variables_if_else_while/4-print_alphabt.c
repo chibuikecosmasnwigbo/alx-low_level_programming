fb1ebf279798c0c34570d81dd0569c5fb8de3ecf
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmail.com>
2023-05-21 07:15:08 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include	<stdio.h>
+/*
+ * main	-	Entry
+ *
+ * Return:	0	(success)
+ *
+ */
+
+
+/*	main	description	*/
+int	main(void)
+{
+	char	ch_a	=	'a';
+	while	(ch_a	<=	'z')
+	{
+		if	(ch_a	!=	'e'	||	ch_a	!=	'q')
+		{
+			putchar(ch_a);
+		}
+		ch_a++;
+	}
+	putchar('\n');
+}
