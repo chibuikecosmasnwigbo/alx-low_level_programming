284e3d95885119e3e5bccd0347987e2bed3f5eb9
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmail.com>
2023-05-21 08:16:29 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include	<stdio.h>
+
+/*
+ * amin	-	Entry
+ *
+ * return:	Always	0	(success)
+ */
+
+int	main(void)
+{
+	char_z	=	'z';
+
+	while	(ch_z	>	'a')
+	{
+		putchar(ch_z);
+		ch_z--;
+	}
+	putchar('\n');
+	return	(0);
+}
