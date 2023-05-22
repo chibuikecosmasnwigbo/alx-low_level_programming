46c2dd9f273e6ed0d0e6755ae0008c1d594de3cd
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmail.com>
2023-05-22 03:47:03 -0700

    print

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include	<stdio.h>
+/*
+ * main	-	Entry
+ *
+ *	Return:	Always	0	(success)
+ */
+
+int	main(void)
+{
+	int	i	=	0;
+	int	j	=	0;
+	
+	while	(i	<	10)
+	{
+		j	=	i	+	1;
+
+		while	(j	<=	9)
+		{
+			putchar(i	+	'0');
+			putchar(j	+	'0');
+			
+			if	(	i	<	10	||	j	<=	9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		j++;
+	}
+		putchar('\n');
+		return	(0);
+}
+
+
+
+ 
