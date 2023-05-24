5ea53149679898f05c891b143ffcce6de214649f
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmail.com>
2023-05-24 03:21:31 -0700

    1 - alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include	"main.h"
 /*
- * print_alphabet	print	all	alphabets	in	lowercase
+ * print_alphabet - print	all	alphabets	in	lowercase
  *
  *
  */
 void print_alphabet(void)
 {
 	char	ch	=	'a';
+
 	while	(ch	<=	'z')
 	{
 		_putchar(ch);
