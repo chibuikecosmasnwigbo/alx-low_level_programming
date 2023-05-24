94749b3706d8fea8097023c5aaadfc99b0b4af94
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmail.com>
2023-05-24 09:01:01 -0700

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,14 +14,11 @@ int	print_sign(int	n)
 		_putchar(98);
 		_putchar(',');
 		return	(1);
-	}
-	else	if	(n	==	0)
+	}else	if	(n	==	0)
 	{
-		_putchar(n	+	'0');
-		_putchar(',');
+		_putchar(48);
 		return	(0);
-	}
-	else	if	(n	<	0);
+	}else	if	(n	<	0)
 	{
 		_putchar('-');
 		_putchar(',');
