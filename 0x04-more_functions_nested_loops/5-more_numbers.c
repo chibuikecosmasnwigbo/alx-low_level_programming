898345f61fbdfd47eee37604fb77924896e0ee9d
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmial.com>
2023-05-25 07:51:02 -0700

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_triangle - a function that prints a triangle
- * @size: size of the triangle
- * Return: triangle of '#'s
+ * more_numbers - a function that prints 10 times the numbers, from 0 to 14
+ * _putchar only 3 times
+ * Return: 0-14 x10 followed by new line
  */
 void more_numbers(void)
 {
 	int i,	j;
-	
+
 	for	(i	=	0;	i	<=	10;	i++)
 	{
 		for	(j	=	0;	j	<=	14;	j++)
