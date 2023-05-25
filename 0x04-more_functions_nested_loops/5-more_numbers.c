987ffa7fccac0bff37b5e6a65baf0243bfb00750
chibuikecosmasnwigbo <chibuikecosmasnwigbo@gmial.com>
2023-05-25 06:07:16 -0700

    update

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_triangle - a function that prints a triangle
+ * @size: size of the triangle
+ * Return: triangle of '#'s
+ */
+void more_numbers(void)
+{
+	int i,	j;
+	
+	for	(i	=	0;	i	<=	10;	i++)
+	{
+		for	(j	=	0;	j	<=	14;	j++)
+		{
+			_putchar(j);
+		}
+	}
+}
